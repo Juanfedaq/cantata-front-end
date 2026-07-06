27791ff694452db31ed50e61ac6a1024c05dc139
juan <juanfedaq@gmail.com>
2026-07-06 10:45:11 -0300

    vs1.1

--- cantata-front-end/vite.config.ts
+++ cantata-front-end/vite.config.ts
@@ -2,17 +2,18 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  // Em produção (GitHub Pages) o site fica em /cantata-front-end/.
+  // No dev local mantém a raiz "/".
+  base: command === 'build' ? '/cantata-front-end/' : '/',
   plugins: [
     vue(),
-    vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+}))
